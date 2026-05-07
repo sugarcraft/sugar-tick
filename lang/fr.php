20a51125a16946818c460b0dbb7b90635e1cc69a
Joe Huss <detain@interserver.net>
2026-05-07 05:35:41 -0400

    Add French locale for all remaining libs (#247)

---
+++ lang/fr.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * French translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'tick envoyé : {project} ({language}) +{duration}s',
+];
