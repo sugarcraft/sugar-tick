aed4f9c2ea3a9e803e6db64bccf27f8aad7f3103
Joe Huss <detain@interserver.net>
2026-05-07 06:00:16 -0400

    Add Dutch locale for all libs (#259)

---
+++ lang/nl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Dutch translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'tick gepusht: {project} ({language}) +{duration}s',
+];
