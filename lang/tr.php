f10640c030d4a1d4a2e3b93781b549241b11c919
Joe Huss <detain@interserver.net>
2026-05-07 06:02:11 -0400

    Add Turkish locale for all libs (#260)

---
+++ lang/tr.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Turkish translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'tick gönderildi: {project} ({language}) +{duration}sn',
+];
