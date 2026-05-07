ce04f0ce3e721e22a94f3e2b90f11fcdda653869
Joe Huss <detain@interserver.net>
2026-05-07 05:32:38 -0400

    Add French locale for candy-freeze (#246)

---
+++ candy-freeze/lang/fr.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * French translations for candy-freeze.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.unknown_flag'  => "candyfreeze : indicateur non reconnu '{flag}'",
+    'cli.unknown_theme' => "candyfreeze : thème inconnu '{name}'. Connus : dark, light, dracula, tokyo-night, nord",
+    'cli.read_failed'   => 'candyfreeze : impossible de lire la donnée',
+    'cli.write_failed'  => "candyfreeze : impossible d'écrire '{path}'",
+];
