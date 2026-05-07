61bec8bf5c722d10ac7846635b3f853e4c980226
Joe Huss <detain@interserver.net>
2026-05-07 05:50:43 -0400

    Add Japanese locale for all libs (#254)

---
+++ lang/ja.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Japanese translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'tick をプッシュしました：{project} ({language}) +{duration}秒',
+];
