605f654d4ca3a91660ca948b52495e16e0dd9d73
Joe Huss <detain@interserver.net>
2026-05-07 05:58:30 -0400

    Add Polish locale for all libs (#258)

---
+++ lang/pl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Polish translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'tick wysłany: {project} ({language}) +{duration}s',
+];
