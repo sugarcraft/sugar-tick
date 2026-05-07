535b1682f5a8c4abd02ec9157da1d073973f17d3
Joe Huss <detain@interserver.net>
2026-05-07 05:47:08 -0400

    Add Brazilian Portuguese locale for all libs (#252)

---
+++ lang/pt-br.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Brazilian Portuguese translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'tick enviado: {project} ({language}) +{duration}s',
+];
