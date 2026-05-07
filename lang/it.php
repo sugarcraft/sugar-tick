3de1f9f9723b6b60cd3e6c5bec79fb9cb74b3b84
Joe Huss <detain@interserver.net>
2026-05-07 17:58:07 -0400

    Add Italian locale for all libs (#256)

---
+++ lang/it.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Italian translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'tick inviato: {project} ({language}) +{duration}s',
+];
