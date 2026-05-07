6a9f41847e9a3fb55603e3aa3eae841e4ceb3007
Joe Huss <detain@interserver.net>
2026-05-07 05:52:41 -0400

    Add Russian locale for all libs (#255)

---
+++ lang/ru.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Russian translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'тик отправлен: {project} ({language}) +{duration}с',
+];
