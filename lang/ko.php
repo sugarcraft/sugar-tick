d949561b27e3bf316121f194786728c8c0aa50db
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Korean translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'tick 푸시됨: {project} ({language}) +{duration}초',
+];
