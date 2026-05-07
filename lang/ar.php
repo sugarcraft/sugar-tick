8043f5cc02cb238f28d50f44da0356d566a98777
Joe Huss <detain@interserver.net>
2026-05-07 06:07:28 -0400

    Add Arabic locale for all libs (#262)

---
+++ lang/ar.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Arabic translations for sugar-tick.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'cli.tick_pushed' => 'تم دفع tick: {project} ({language}) +{duration}ث',
+];
